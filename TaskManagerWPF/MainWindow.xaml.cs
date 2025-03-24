1f69eee2259ace9fd9ff3d634db4ff86add825c3
RuMe6 <rustam.meshkin@gmail.com>
2025-03-24 19:33:18 +0500

    The WPF solution for TaskManager

---
+++ TaskManagerWPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskManager;
+
+
+namespace TaskManagerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TaskContext context = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = context;
+        }
+
+        private void AddTask(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Enter the data of task");
+        }
+    }
+}
